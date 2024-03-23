99e00c2040eeaf5d5f78419488ee9293c914e99e
harishsadasivuni <harishsadasivuni@gmail.com>
2024-03-23 16:33:03 -0400

    added form ui and animation

--- src/app/formPost/formPost.component.ts
+++ src/app/formPost/formPost.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild, } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpClient } from '@angular/common/http';
 
@@ -8,12 +8,21 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./formPost.component.css']
 })
 export class FormPostComponent implements OnInit {
-
+  @ViewChild('alertid', { static: false }) public mydiv: ElementRef;
   constructor(private http:HttpClient) { }
   posturl:string = "http://localhost:8080/addusers";
   formstaticsubmit(formdata: HTMLInputElement){
     this.http.post(this.posturl, formdata)
-    .subscribe();
+    .subscribe(resp => 
+      {if(resp == true){
+        alert("Form submitted succesfully");
+        let y = document.getElementById("loginform");
+        let x = document.getElementById("alertid");
+        y!.style.display = "none";
+        x!.style.display = "block";
+      }else{
+        alert("Failed to submit form");
+      }});
     }
 
   
